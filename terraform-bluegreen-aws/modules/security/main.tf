a3c1a6cf89e44e19321547ea5a89610f12192be4
gopal1409 <gopal1409@gmail.com>
2026-08-25 11:06:08 +0530

    refactor(security): use variables for ingress and egress rules

--- terraform-bluegreen-aws/modules/security/main.tf
+++ terraform-bluegreen-aws/modules/security/main.tf
@@ -1,68 +1,69 @@
-locals {
-  web_ports = [22, 80, 443]
-}
-
 resource "aws_security_group" "web_sg" {
-  name   = "${var.project}-${var.environment}-web-sg"
-  vpc_id = var.vpc_id
-  dynamic "ingress" {
-    for_each = local.web_ports
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-
-    }
-
-  }
-  egress {
-    
-    
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-
-      }
-      tags = {
-    Name = "${var.project}-${var.environment}-web-sg"
-   
-  }
-}
-###############################
-###alb-security-group
-##############################
-resource "aws_security_group" "alb_sg" {
-  name        = "${var.project}-${var.environment}-alb-sg"
-  description = "allow http traffic to alb"
+  name        = "${var.project}-${var.environment}-web-sg"
+  description = "Security group for web instances"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.web_ingress_rules
+
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+  dynamic "egress" {
+    for_each = var.egress_rules
+
+    content {
+      description      = egress.value.description
+      from_port        = egress.value.from_port
+      to_port          = egress.value.to_port
+      protocol         = egress.value.protocol
+      cidr_blocks      = egress.value.cidr_blocks
+      ipv6_cidr_blocks = egress.value.ipv6_cidr_blocks
+    }
   }
-  tags = {
+
+  tags = merge(var.tags, {
+    Name = "${var.project}-${var.environment}-web-sg"
+  })
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project}-${var.environment}-alb-sg"
+  description = "Security group for Application Load Balancer"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.alb_ingress_rules
+
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.egress_rules
+
+    content {
+      description      = egress.value.description
+      from_port        = egress.value.from_port
+      to_port          = egress.value.to_port
+      protocol         = egress.value.protocol
+      cidr_blocks      = egress.value.cidr_blocks
+      ipv6_cidr_blocks = egress.value.ipv6_cidr_blocks
+    }
+  }
+
+  tags = merge(var.tags, {
     Name = "${var.project}-${var.environment}-alb-sg"
-   
-  }
+  })
 }
