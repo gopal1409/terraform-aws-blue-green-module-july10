49f01d1dd0a62acf85d91414d8db8c383cbeb04d
gopal1409 <gopal1409@gmail.com>
2026-08-25 11:19:45 +0530

    fix(security): return security group IDs and ARNs

--- terraform-bluegreen-aws/modules/security/output.tf
+++ terraform-bluegreen-aws/modules/security/output.tf
@@ -1,8 +1,19 @@
 output "web_sg_id" {
-  description = "web_security group"
-  value = aws_security_group.web_sg
+  description = "ID of the web/EC2 security group."
+  value       = aws_security_group.web_sg.id
 }
+
+output "web_sg_arn" {
+  description = "ARN of the web/EC2 security group."
+  value       = aws_security_group.web_sg.arn
+}
+
 output "alb_sg_id" {
-  description = "alb security group"
-  value = aws_security_group.alb_sg
+  description = "ID of the Application Load Balancer security group."
+  value       = aws_security_group.alb_sg.id
+}
+
+output "alb_sg_arn" {
+  description = "ARN of the Application Load Balancer security group."
+  value       = aws_security_group.alb_sg.arn
 }
