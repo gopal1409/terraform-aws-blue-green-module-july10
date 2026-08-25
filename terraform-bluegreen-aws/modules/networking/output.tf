3c04d2f9122809567bddec8e9bb3876d8a76426d
gopal1409 <gopal1409@gmail.com>
2026-08-25 11:19:39 +0530

    fix(networking): add subnet list outputs for module composition

--- terraform-bluegreen-aws/modules/networking/output.tf
+++ terraform-bluegreen-aws/modules/networking/output.tf
@@ -1,18 +1,28 @@
 output "vpc_id" {
-  value = aws_vpc.this.id
+  description = "ID of the VPC."
+  value       = aws_vpc.this.id
 }
 
 output "public_subnet_ids" {
+  description = "Map of public subnet names to subnet IDs."
   value = {
-    for k,v in aws_subnet.public :
-    k => v.id
+    for k, v in aws_subnet.public : k => v.id
   }
 }
 
+output "public_subnet_id_list" {
+  description = "List of public subnet IDs, suitable for passing to load balancer modules."
+  value       = values(aws_subnet.public)[*].id
+}
+
 output "private_subnet_ids" {
+  description = "Map of private subnet names to subnet IDs."
   value = {
-    for k,v in aws_subnet.private :
-    k => v.id
+    for k, v in aws_subnet.private : k => v.id
   }
 }
-##newline
+
+output "private_subnet_id_list" {
+  description = "List of private subnet IDs, suitable for passing to EC2 modules."
+  value       = values(aws_subnet.private)[*].id
+}
