cc1d66a271b5bfef85db6abb54ed500b981c1e1d
gopal1409 <gopal1409@gmail.com>
2026-08-25 11:06:02 +0530

    refactor(security): make security group rules configurable

--- terraform-bluegreen-aws/modules/security/variables.tf
+++ terraform-bluegreen-aws/modules/security/variables.tf
@@ -1,17 +1,103 @@
 variable "project" {
-  
+  description = "Project name used in security group names and tags."
+  type        = string
 }
 
 variable "environment" {
-  
+  description = "Environment name used in security group names and tags."
+  type        = string
 }
 
-variable "vpc_cidr" {
-  
-}
 variable "vpc_id" {
-  
+  description = "VPC ID where the security groups will be created."
+  type        = string
 }
+
+variable "web_ingress_rules" {
+  description = "Ingress rules for the web security group."
+  type = list(object({
+    description = optional(string)
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = optional(list(string), [])
+  }))
+  default = [
+    {
+      description = "SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "HTTPS"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "alb_ingress_rules" {
+  description = "Ingress rules for the ALB security group."
+  type = list(object({
+    description = optional(string)
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = optional(list(string), [])
+  }))
+  default = [
+    {
+      description = "HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "HTTPS"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "egress_rules" {
+  description = "Egress rules applied to both security groups."
+  type = list(object({
+    description      = optional(string)
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = optional(list(string), [])
+    ipv6_cidr_blocks = optional(list(string), [])
+  }))
+  default = [
+    {
+      description      = "Allow all outbound traffic"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+}
+
 variable "tags" {
-  type = map(string)
+  description = "Additional tags applied to the security groups."
+  type        = map(string)
+  default     = {}
 }
